b95b41f1808595aa06ec7fd9e2b17be3f40f627a
AmanKesarwani47 <amankesarwani34@gmail.com>
2026-09-12 16:00:37 +0530

    Add CD workflow for auto deployment

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.routers import users, tasks, auth
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="TaskSphere API")
+app = FastAPI(title="TaskSphere Backend")
 
 app.include_router(auth.router)
 app.include_router(users.router)
