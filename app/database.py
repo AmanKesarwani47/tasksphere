e025c8165e9ed47fc2d2918d1afa51a5b6e09263
AmanKesarwani47 <amankesarwani34@gmail.com>
2026-09-13 14:19:35 +0530

    Connected postgres to fastapi

--- app/database.py
+++ app/database.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///./tasksphere.db"
+from app.config import settings
+
+DATABASE_URL = os.getenv("DATABASE_URL", settings.DATABASE_URL)
 
 Base = declarative_base()
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
